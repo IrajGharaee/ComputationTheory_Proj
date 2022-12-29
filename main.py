c1e9ec1cffe21507a46b883a2892a69cdc97ea86
IrajGharaee <irajgh5533@gmail.com>
2022-12-29 23:42:33 +0330

    is_string_valid edited

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class State:
         self.name = name
 
     def set_transitions(self, dest, alph):
-        self.transitions.update({dest: alph})
+        self.transitions.update({alph: dest})
 
 
 class Automata:
@@ -15,46 +15,47 @@ class Automata:
     accepts = []
     alphabet = []
 
-    def __init__(self, states_passed, accepts_passed, alphabet_passed, q0_passed, func):
+    def __init__(self, states_passed, accepts_passed, alphabet_passed, q0_passed, function):
         self.set_states(states_passed)
         self.set_accepts(accepts_passed)
         self.alphabet = alphabet_passed
         self.q0 = State(q0_passed)
-        self.set_func(func)
+        self.set_func(function)
 
     def set_states(self, states_passed):
         for state in states_passed:
-            self.states = states.append(State(states))
+            self.states = self.states.append(State(self.states))
 
     def set_accepts(self, accepts_passed):
-        for accept in accepts_passed:
-            accept_temp = states[states.index(accept)]
+        for a in accepts_passed:
+            accept_temp = self.states[self.states.index(a)]
             accept_temp.isAccept = True
-            self.accepts.append()
+            self.accepts.append(accept_temp)
 
-    def set_func(self, func):
-        for i in range(0,int(len(func)/3)):
-            src = func[i]
-            dest = func[i + 1]
-            alph = func[i + 2]
-            states[states.index(src)].set_transition(dest,alph)
+    def set_func(self, function):
+        for i in range(0, int(len(function)/3)):
+            src = self.states.index(function[i * 3])
+            dest = self.states.index(function[(i * 3) + 1])
+            alph = self.states.index(function[(i * 3) + 2])
+            self.states[src].set_transition(dest, alph)
     
     def is_string_valid(self, string):
         
         inputs = list(string)
         current_state = self.q0
-        for letter in inputs :
+        for letter in inputs:
             try:
-                if letter in self.set_transitions[current_state]:
-                    current_state = self.set_transitions[current_state][letter]
+                if letter in current_state.transitions:
+                    current_state = current_state.transitions[letter]
                 else:
                     return False
-            except KeyError as e :
+            except KeyError as e:
                 return False
         if current_state in self.accepts:
             return True
-        
+
         return False
+
 
 states = input('Enter states:')
 accept = input('Enter accepting states:')
