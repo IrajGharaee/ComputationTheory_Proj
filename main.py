fc7a02146ed69ee5aa7a64c8da01014a43f89831
Sarazoghi <92922906+Sarazoghi@users.noreply.github.com>
2022-12-29 23:21:21 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -15,30 +15,46 @@ class Automata:
     accepts = []
     alphabet = []
 
-    def __init__(self, states_passed, accepts_passed, alphabet_passed, q0_passed, function):
+    def __init__(self, states_passed, accepts_passed, alphabet_passed, q0_passed, func):
         self.set_states(states_passed)
         self.set_accepts(accepts_passed)
         self.alphabet = alphabet_passed
         self.q0 = State(q0_passed)
-        self.set_func(function)
+        self.set_func(func)
 
     def set_states(self, states_passed):
         for state in states_passed:
-            self.states = self.states.append(State(self.states))
+            self.states = states.append(State(states))
 
     def set_accepts(self, accepts_passed):
-        for a in accepts_passed:
-            accept_temp = self.states[self.states.index(a)]
+        for accept in accepts_passed:
+            accept_temp = states[states.index(accept)]
             accept_temp.isAccept = True
-            self.accepts.append(accept_temp)
+            self.accepts.append()
 
-    def set_func(self, function):
-        for i in range(0, int(len(function)/3)):
-            src = function[i*3]
-            dest = function[(i * 3) + 1]
-            alph = function[(i * 3) + 2]
-            self.states[self.states.index(src)].set_transition(dest, alph)
-
+    def set_func(self, func):
+        for i in range(0,int(len(func)/3)):
+            src = func[i]
+            dest = func[i + 1]
+            alph = func[i + 2]
+            states[states.index(src)].set_transition(dest,alph)
+    
+    def is_string_valid(self, string):
+        
+        inputs = list(string)
+        current_state = self.q0
+        for letter in inputs :
+            try:
+                if letter in self.set_transitions[current_state]:
+                    current_state = self.set_transitions[current_state][letter]
+                else:
+                    return False
+            except KeyError as e :
+                return False
+        if current_state in self.accepts:
+            return True
+        
+        return False
 
 states = input('Enter states:')
 accept = input('Enter accepting states:')
