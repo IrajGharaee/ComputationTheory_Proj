9048bdb47374e528e9cb46c6bda006b9df451726
IrajGharaee <irajgh5533@gmail.com>
2022-12-29 15:19:49 +0330

    Automata and States classes added

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+class State:
+    name = ''
+    isAccept = False
+    transitions = {}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, name):
+        self.name = name
+
+    def set_transitions(self, dest, alph):
+        self.transitions.update({dest: alph})
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Automata:
+    states = []
+    accepts = []
+    alphabet = []
+
+    def __init__(self, states_passed, accepts_passed, alphabet_passed, q0_passed, func):
+        self.set_states(states_passed)
+        self.set_accepts(accepts_passed)
+        self.alphabet = alphabet_passed
+        self.q0 = State(q0_passed)
+        self.set_func(func)
+
+    def set_states(self, states_passed):
+        for state in states_passed:
+            self.states = states.append(State(states))
+
+    def set_accepts(self, accepts_passed):
+        for accept in accepts_passed:
+            accept_temp = states[states.index(accept)]
+            accept_temp.isAccept = True
+            self.accepts.append()
+
+    def set_func(self, func):
+        for i in range(0,int(len(func)/3)):
+            src = func[i]
+            dest = func[i + 1]
+            alph = func[i + 2]
+            states[states.index(src)].set_transition(dest,alph)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+states = input('Enter states:')
+accept = input('Enter accepting states:')
+alphabet = input('Enter alphabet:')
+q0 = input('Enter first state:')
+func = input('Enter transition functions. From state i to state j by alphabet a -> i j a:')
+automata = Automata(states, accept, alphabet, q0, func)
